f685b5af53327114b855abcc4335aef140ea709c
yantang <yantang@microsoft.com>
2017-04-20 13:36:47 -0700

    HostingDiagnosticListener listen to both old and new events

--- src/Microsoft.ApplicationInsights.AspNetCore/DiagnosticListeners/Implementation/HostingDiagnosticListener.cs
+++ src/Microsoft.ApplicationInsights.AspNetCore/DiagnosticListeners/Implementation/HostingDiagnosticListener.cs
@@ -3,9 +3,12 @@
     using System;
     using System.Diagnostics;
     using System.Net.Http.Headers;
+    using System.Reflection;
+    using Extensibility.Implementation.Tracing;
     using Microsoft.ApplicationInsights.AspNetCore.Extensions;
     using Microsoft.ApplicationInsights.DataContracts;
     using Microsoft.ApplicationInsights.Extensibility.Implementation;
+    using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.DiagnosticAdapter;
     using Microsoft.Extensions.Primitives;
@@ -18,6 +21,7 @@
         private readonly TelemetryClient client;
         private readonly ICorrelationIdLookupHelper correlationIdLookupHelper;
         private readonly string sdkVersion;
+        private bool? isAspNetCore20;
         private const string ActivityCreatedByHostingDiagnosticListener = "ActivityCreatedByHostingDiagnosticListener";
 
         /// <summary>
@@ -36,70 +40,67 @@
         public string ListenerName { get; } = "Microsoft.AspNetCore";
 
         /// <summary>
+        /// Determine whether the running AspNetCore Hosting version is 2.0 or higher. This will affect what DiagnosticSource events we receive.
+        /// To support AspNetCore 1.0 and 2.0, we listen to both old and new events.
+        /// If the running AspNetCore version is 2.0, both old and new events will be sent. In this case, we will ignore the old events.
+        /// </summary>
+        public bool IsAspNetCore20
+        {
+            get
+            {
+                if (isAspNetCore20.HasValue)
+                {
+                    return isAspNetCore20.Value;
+                }
+
+                var hostingVersion = typeof(WebHostBuilder).GetTypeInfo().Assembly.GetName().Version;
+                isAspNetCore20 = hostingVersion.Major >= 2;
+
+                return isAspNetCore20.Value;
+            }
+        }
+
+        /// <summary>
+        /// Diagnostic event handler method for 'Microsoft.AspNetCore.Hosting.HttpRequestIn' event.
+        /// </summary>
+        [DiagnosticName("Microsoft.AspNetCore.Hosting.HttpRequestIn")]
+        public void OnHttpRequestIn()
+        {
+            // do nothing, just enable the diagnotic source
+        }
+
+        /// <summary>
+        /// Diagnostic event handler method for 'Microsoft.AspNetCore.Hosting.HttpRequestIn.Start' event.
+        /// </summary>
+        [DiagnosticName("Microsoft.AspNetCore.Hosting.HttpRequestIn.Start")]
+        public void OnHttpRequestInStart(HttpContext httpContext)
+        {
+            if (this.client.IsEnabled())
+            {
+                var requestTelemetry = InitializeRequestTelemetryOnHttpRequestInStart(httpContext);
+                SetAppIdInResponseHeader(httpContext, requestTelemetry);
+            }
+        }
+
+        /// <summary>
+        /// Diagnostic event handler method for 'Microsoft.AspNetCore.Hosting.HttpRequestIn.Stop' event.
+        /// </summary>
+        [DiagnosticName("Microsoft.AspNetCore.Hosting.HttpRequestIn.Stop")]
+        public void OnHttpRequestInStop(HttpContext httpContext)
+        {
+            EndRequest(httpContext, Stopwatch.GetTimestamp());
+        }
+
+        /// <summary>
         /// Diagnostic event handler method for 'Microsoft.AspNetCore.Hosting.BeginRequest' event.
         /// </summary>
         [DiagnosticName("Microsoft.AspNetCore.Hosting.BeginRequest")]
         public void OnBeginRequest(HttpContext httpContext, long timestamp)
         {
-            if (this.client.IsEnabled())
+            if (this.client.IsEnabled() && !IsAspNetCore20)
             {
-                var requestTelemetry = new RequestTelemetry();
-                var activity = new Activity(ActivityCreatedByHostingDiagnosticListener);
-
-                StringValues requestId;
-                StringValues standardRootId;
-                if (httpContext.Request.Headers.TryGetValue(RequestResponseHeaders.RequestIdHeader, out requestId))
-                {
-                    activity.SetParentId(requestId);
-                    requestTelemetry.Context.Operation.ParentId = requestId;
-
-                    string[] baggage = httpContext.Request.Headers.GetCommaSeparatedValues(RequestResponseHeaders.CorrelationContextHeader);
-                    if (baggage != StringValues.Empty)
-                    {
-                        foreach (var item in baggage)
-                        {
-                            NameValueHeaderValue baggageItem;
-                            if (NameValueHeaderValue.TryParse(item, out baggageItem))
-                            {
-                                activity.AddBaggage(baggageItem.Name, baggageItem.Value);
-                                if (!requestTelemetry.Context.Properties.ContainsKey(baggageItem.Name))
-                                {
-                                    requestTelemetry.Context.Properties[baggageItem.Name] = baggageItem.Value;
-                                }
-                            }
-                        }
-                    }
-                }
-                else if (httpContext.Request.Headers.TryGetValue(RequestResponseHeaders.StandardRootIdHeader, out standardRootId))
-                {
-                    activity.SetParentId(standardRootId);
-
-                    StringValues standardParentId;
-                    if (httpContext.Request.Headers.TryGetValue(RequestResponseHeaders.StandardParentIdHeader, out standardParentId))
-                    {
-                        requestTelemetry.Context.Operation.ParentId = standardParentId;
-                    }
-                }
-                activity.Start();
-                httpContext.Features.Set(activity);
-
-                requestTelemetry.Id = activity.Id;
-                requestTelemetry.Context.Operation.Id = activity.RootId;
-
-                this.client.Initialize(requestTelemetry);
-                requestTelemetry.Start(timestamp);
-                httpContext.Features.Set(requestTelemetry);
-                IHeaderDictionary responseHeaders = httpContext.Response?.Headers;
-                if (responseHeaders != null &&
-                    !string.IsNullOrEmpty(requestTelemetry.Context.InstrumentationKey) &&
-                    (!responseHeaders.ContainsKey(RequestResponseHeaders.RequestContextHeader) || HttpHeadersUtilities.ContainsRequestContextKeyValue(responseHeaders, RequestResponseHeaders.RequestContextTargetKey)))
-                {
-                    string correlationId = null;
-                    if (this.correlationIdLookupHelper.TryGetXComponentCorrelationId(requestTelemetry.Context.InstrumentationKey, out correlationId))
-                    {
-                        HttpHeadersUtilities.SetRequestContextKeyValue(responseHeaders, RequestResponseHeaders.RequestContextTargetKey, correlationId);
-                    }
-                }
+                var requestTelemetry = InitializeRequestTelemetryOnBeginRequest(httpContext, timestamp);
+                SetAppIdInResponseHeader(httpContext, requestTelemetry);
             }
         }
 
@@ -109,7 +110,10 @@
         [DiagnosticName("Microsoft.AspNetCore.Hosting.EndRequest")]
         public void OnEndRequest(HttpContext httpContext, long timestamp)
         {
-            EndRequest(httpContext, timestamp);
+            if (!IsAspNetCore20)
+            {
+                EndRequest(httpContext, timestamp);
+            }
         }
 
         /// <summary>
@@ -119,7 +123,13 @@
         public void OnHostingException(HttpContext httpContext, Exception exception)
         {
             this.OnException(httpContext, exception);
-            this.EndRequest(httpContext, Stopwatch.GetTimestamp());
+
+            // In AspNetCore 1.0, when an exception is unhandled, it will only send UnhandledException event but not EndRequest event, so we need to EndRequest at here.
+            // In AspNetCore 2.0, after sending UnhandledException, it will stop the created activity, which will send HttpRequestIn.Stop event, so we just EndRequest at there.
+            if (!IsAspNetCore20)
+            {
+                this.EndRequest(httpContext, Stopwatch.GetTimestamp());
+            }
         }
 
         /// <summary>
@@ -138,6 +148,124 @@
         public void OnDiagnosticsUnhandledException(HttpContext httpContext, Exception exception)
         {
             this.OnException(httpContext, exception);
+        }
+
+        private RequestTelemetry InitializeRequestTelemetryOnHttpRequestInStart(HttpContext httpContext)
+        {
+            var requestTelemetry = new RequestTelemetry();
+
+            if (Activity.Current == null)
+            {
+                AspNetCoreEventSource.Instance.LogHostingDiagnosticListenerOnHttpRequestInStartActivityNull();
+            }
+            else if (Activity.Current.ParentId != null)
+            {
+                // The activity is initialized from the new "Request-Id" header
+                requestTelemetry.Context.Operation.ParentId = Activity.Current.ParentId;
+
+                foreach (var prop in Activity.Current.Baggage)
+                {
+                    if (!requestTelemetry.Context.Properties.ContainsKey(prop.Key))
+                    {
+                        requestTelemetry.Context.Properties[prop.Key] = prop.Value;
+                    }
+                }
+            }
+            else
+            {
+                // The request doesn't have the new "Request-Id" header but the standard headers,
+                // we create a new activity and initialize it by the standard headers.
+                StringValues xmsRequestRootId;
+                if (httpContext.Request.Headers.TryGetValue(RequestResponseHeaders.StandardRootIdHeader, out xmsRequestRootId))
+                {
+                    var activity = new Activity(ActivityCreatedByHostingDiagnosticListener);
+                    activity.SetParentId(xmsRequestRootId);
+                    activity.Start();
+                    httpContext.Features.Set(activity);
+                }
+
+                StringValues xmsRequestId;
+                if (httpContext.Request.Headers.TryGetValue(RequestResponseHeaders.StandardParentIdHeader, out xmsRequestId))
+                {
+                    requestTelemetry.Context.Operation.ParentId = xmsRequestId;
+                }
+            }
+
+            requestTelemetry.Id = Activity.Current?.Id;
+            requestTelemetry.Context.Operation.Id = Activity.Current?.RootId;
+
+            this.client.Initialize(requestTelemetry);
+            requestTelemetry.Start();
+            httpContext.Features.Set(requestTelemetry);
+
+            return requestTelemetry;
+        }
+
+        private RequestTelemetry InitializeRequestTelemetryOnBeginRequest(HttpContext httpContext, long timestamp)
+        {
+            var requestTelemetry = new RequestTelemetry();
+            var activity = new Activity(ActivityCreatedByHostingDiagnosticListener);
+
+            StringValues requestId;
+            StringValues standardRootId;
+            if (httpContext.Request.Headers.TryGetValue(RequestResponseHeaders.RequestIdHeader, out requestId))
+            {
+                activity.SetParentId(requestId);
+                requestTelemetry.Context.Operation.ParentId = requestId;
+
+                string[] baggage = httpContext.Request.Headers.GetCommaSeparatedValues(RequestResponseHeaders.CorrelationContextHeader);
+                if (baggage != StringValues.Empty)
+                {
+                    foreach (var item in baggage)
+                    {
+                        NameValueHeaderValue baggageItem;
+                        if (NameValueHeaderValue.TryParse(item, out baggageItem))
+                        {
+                            activity.AddBaggage(baggageItem.Name, baggageItem.Value);
+                            if (!requestTelemetry.Context.Properties.ContainsKey(baggageItem.Name))
+                            {
+                                requestTelemetry.Context.Properties[baggageItem.Name] = baggageItem.Value;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (httpContext.Request.Headers.TryGetValue(RequestResponseHeaders.StandardRootIdHeader, out standardRootId))
+            {
+                activity.SetParentId(standardRootId);
+
+                StringValues standardParentId;
+                if (httpContext.Request.Headers.TryGetValue(RequestResponseHeaders.StandardParentIdHeader, out standardParentId))
+                {
+                    requestTelemetry.Context.Operation.ParentId = standardParentId;
+                }
+            }
+            activity.Start();
+            httpContext.Features.Set(activity);
+
+            requestTelemetry.Id = activity.Id;
+            requestTelemetry.Context.Operation.Id = activity.RootId;
+
+            this.client.Initialize(requestTelemetry);
+            requestTelemetry.Start(timestamp);
+            httpContext.Features.Set(requestTelemetry);
+
+            return requestTelemetry;
+        }
+
+        private void SetAppIdInResponseHeader(HttpContext httpContext, RequestTelemetry requestTelemetry)
+        {
+            IHeaderDictionary responseHeaders = httpContext.Response?.Headers;
+            if (responseHeaders != null &&
+                !string.IsNullOrEmpty(requestTelemetry.Context.InstrumentationKey) &&
+                (!responseHeaders.ContainsKey(RequestResponseHeaders.RequestContextHeader) || HttpHeadersUtilities.ContainsRequestContextKeyValue(responseHeaders, RequestResponseHeaders.RequestContextTargetKey)))
+            {
+                string correlationId = null;
+                if (this.correlationIdLookupHelper.TryGetXComponentCorrelationId(requestTelemetry.Context.InstrumentationKey, out correlationId))
+                {
+                    HttpHeadersUtilities.SetRequestContextKeyValue(responseHeaders, RequestResponseHeaders.RequestContextTargetKey, correlationId);
+                }
+            }
         }
 
         private void EndRequest(HttpContext httpContext, long timestamp)
