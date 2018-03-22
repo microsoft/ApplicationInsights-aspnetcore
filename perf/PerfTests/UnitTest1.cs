be04888dc8d49f31263a711bef73cb03ed331b5b
Cijo Thomas <cithomas@microsoft.com>
2018-03-22 08:08:57 -0700

    Additional traces from perf test

--- perf/PerfTests/UnitTest1.cs
+++ perf/PerfTests/UnitTest1.cs
@@ -3,6 +3,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
+using System.Threading;
 
 namespace PerfTests
 {
@@ -16,9 +17,15 @@ namespace PerfTests
         public void TestMethod1()
         {
             var s = Directory.GetCurrentDirectory();
-            PerfMeasurements perfMeasurements1 = MeasureApp("..\\..\\..\\..\\artifacts\\perf\\App1\\netcoreapp2.0\\App1.dll");
+            Trace.WriteLine("Current Dir:" + s);
 
+            Trace.WriteLine("Launching App1");
+            PerfMeasurements perfMeasurements1 = MeasureApp("..\\..\\..\\..\\artifacts\\perf\\App1\\netcoreapp2.0\\App1.dll");
+            PrintPerfMeasurements(perfMeasurements1);
+
+            Trace.WriteLine("Launching App2");
             PerfMeasurements perfMeasurements2 = MeasureApp("..\\..\\..\\..\\artifacts\\perf\\App2\\netcoreapp2.0\\App2.dll");
+            PrintPerfMeasurements(perfMeasurements1);
 
             double overhead = ((perfMeasurements1.rpsPerCpu - perfMeasurements2.rpsPerCpu) / perfMeasurements2.rpsPerCpu) * 100;
 
@@ -27,12 +34,20 @@ namespace PerfTests
 
         }
 
+        private static void PrintPerfMeasurements(PerfMeasurements perfMeasurements)
+        {
+            Trace.WriteLine("Rps:" + perfMeasurements.rps);
+            Trace.WriteLine("Cpu:" + perfMeasurements.cpuAverage);
+            Trace.WriteLine("RpsPerCpu:" + perfMeasurements.rpsPerCpu);
+        }
+
         private static PerfMeasurements MeasureApp(string pathToApp)
         {
             // Launch App
             Process app = CommandLineHelpers.ExecuteCommand("dotnet", pathToApp, true);
             app.ProcessorAffinity = (IntPtr)12;
             app.PriorityClass = ProcessPriorityClass.High;
+            Trace.WriteLine("ProcessId:" + app.Id);
 
             //Verify App
             HttpClient client = new HttpClient();
@@ -44,16 +59,19 @@ namespace PerfTests
                 TargetRps, TestDuration));
             loadGenProcess.ProcessorAffinity = (IntPtr)3;
             loadGenProcess.PriorityClass = ProcessPriorityClass.Normal;
+            Trace.WriteLine("ProcessId (loadgen):" + loadGenProcess.Id);
 
             // Launch perf counter reader
             Process MeasureCounterProcess = CommandLineHelpers.ExecuteCommand("powershell",
             ".\\ReadCounter.ps1");
             string avgCpu = MeasureCounterProcess.StandardOutput.ReadToEnd();
             MeasureCounterProcess.WaitForExit();
+            Trace.WriteLine("AvgCpu:" + avgCpu);
 
 
             string requCount = loadGenProcess.StandardOutput.ReadToEnd();
             loadGenProcess.WaitForExit();
+            Trace.WriteLine("Total requests:" + requCount);
 
             double totalRequests = Math.Round(double.Parse(requCount), 2);
             double cpuAverage = Math.Round(double.Parse(avgCpu), 2);
@@ -63,7 +81,9 @@ namespace PerfTests
 
             double rpsPerCpu = Math.Round(rps / cpuAverage, 2);
 
-            app.Kill();
+            app.Kill();            
+            Thread.Sleep(1000);
+            Trace.WriteLine(app.Id + " existed? :" + app.HasExited);
 
             return new PerfMeasurements()
             {
