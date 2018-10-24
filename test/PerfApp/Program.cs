025251dc01252477ed111fb8b9d7827cfcbaf88c
Cijo Thomas <cithomas@microsoft.com>
2018-10-24 11:52:22 -0700

    Add perf app

---
+++ test/PerfApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace PerfApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                // .UseApplicationInsights("b5feab90-214e-445d-9ea5-3a15339904dd")
+                .Build();
+    }
+}
