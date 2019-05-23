03c918d1b6d269ed9aee4cf020ea8284a912a11e
Timothy Mothra Lee <tilee@microsoft.com>
2019-05-23 16:18:04 -0700

    update xml comments

--- src/Microsoft.ApplicationInsights.AspNetCore/TelemetryInitializers/ComponentVersionTelemetryInitializer.cs
+++ src/Microsoft.ApplicationInsights.AspNetCore/TelemetryInitializers/ComponentVersionTelemetryInitializer.cs
@@ -12,11 +12,16 @@
     {
         private readonly string version;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ComponentVersionTelemetryInitializer" /> class.
+        /// </summary>
+        /// <param name="options">Provides the Application Version to be added to the telemetry.</param>
         public ComponentVersionTelemetryInitializer(IOptions<ApplicationInsightsServiceOptions> options)
         {
              this.version = options.Value.ApplicationVersion;
         }
 
+        /// <inheritdoc />
         public void Initialize(ITelemetry telemetry)
         {
             if (string.IsNullOrEmpty(telemetry.Context.Component.Version))
